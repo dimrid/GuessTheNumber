206bb32566a4f8c32dc081bca28cd8f310f42382
dimrid <didishic@gmail.com>
2023-05-29 20:55:54 +0300

    Edited code

--- Start.cs
+++ Start.cs
@@ -9,19 +9,17 @@ using System.Threading.Tasks;
 
 namespace Guess_the_Number
 {
-    class Program
+    class Start
     {
         public static void Main()
         {
-            GetGreetings();
-
-            int choiseUser;
+            GetGreetings(); // The first step is greetings
 
             string userInput = "";
 
             while (true)
             {
-                switch (GetUserCorrectAnswer(userInput))
+                switch (GetUserCorrectAnswer(userInput)) // The second step is getting an answer from the user
                 {
                     case 1:
                         PlayerGuesses();
@@ -29,11 +27,51 @@ namespace Guess_the_Number
                     case 2:
                         ComputerGuesses();
                         break;
-                    default:
-
-                        break;
                 }
             }
+
+        }
+        private static void GetGreetings()
+        {
+            Console.WriteLine("Hello! Let's start playing!");
+
+            Console.WriteLine("You need to guess the number from 0 to 100.");
+
+            Console.WriteLine("Who is playing?\nIf human vs. human, enter: 1.\nIf human vs. computer enter: 2.");
+        }
+        static int GetUserCorrectAnswer(string input)
+        {
+            input = Console.ReadLine();
+
+            if (int.TryParse(input, out int result) && (result == 1 || result == 2))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("You're wrong! Try again!");
+            }
+            return result;
+        }
+
+        static void ComputerGuesses()
+        {
+            Console.WriteLine("I guessed a number. Try to guess");
+
+            Random rnd = new Random();
+
+            int guessed = rnd.Next(0, 100);
+
+            int tries = 5;
+
+            while (tries < 5 || guessed == GetUserCorrectAnswer(""))
+            {
+                Console.WriteLine($"MY CONGRATULATIONS!!!! YOU WIN!! My number is {guessed}");
+            }
+
+        }
+        static void PlayerGuesses()
+        {
             int index = 0;
 
             string player_2 = Console.ReadLine();
@@ -64,39 +102,6 @@ namespace Guess_the_Number
                     Console.WriteLine("NO! Please. Try again");
                 }
             }
-
-        }
-
-        private static void GetGreetings()
-        {
-            Console.WriteLine("Hello! Let's start playing!");
-
-            Console.WriteLine("You need to guess the number from 0 to 100.");
-
-            Console.WriteLine("Who is playing?\nIf human vs. human, enter: 1.\nIf human vs. computer enter: 2.");
-        }
-
-        static int GetUserCorrectAnswer(string input)
-        {
-            input = Console.ReadLine();
-
-            if (int.TryParse(input, out int result) && (result == 1 || result == 2))
-            {
-                return result;
-            }
-            else
-            {
-                Console.WriteLine("You're wrong! Try again!");
-            }
-            return result;
-        }
-
-        private static void ComputerGuesses()
-        {
-            Console.WriteLine("Comp");
-        }
-        private static void PlayerGuesses()
-        {
             Console.WriteLine("Human");
             Console.WriteLine("Please, PLAYER 1 enter the number:");
 
@@ -109,30 +114,6 @@ namespace Guess_the_Number
             Console.WriteLine("I've got your number.");
 
             Console.WriteLine("Please, PLAYER 2 enter the number:");
-        }
-
-
-        static int GetNumber()
-        {
-            Random random = new Random();
-
-
-            int randNum = random.Next(0, 100);
-
-            return randNum;
-        }
-        int GetArrays(int player)
-        {
-            int[] numbers = new int[101];
-
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbers[0 + i] = i;
-            }
-
-            int index = Array.BinarySearch(numbers, player);
-
-            return index;
         }
 
     }
